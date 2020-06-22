3716df859d37edd58967d5a96ebf653f6ebd8c15
Bobby Uno <robert.y.uno@gmail.com>
2020-06-22 14:41:27 -0700

    added 2 paraphs to UserOutput

--- assignment-1/src/App.js
+++ assignment-1/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './Components/UserInput'
+import UserOutput from './Components/UserOutput'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Assignment 1 </h1>
+      <UserOutput/>
+      <UserOutput/>
+      <UserOutput/>
+
     </div>
   );
 }
---
+++ assignment-1/src/Components/UserOutput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const userOutput = (props) => {
+
+  return (
+    <div>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mauris metus, interdum nec orci pellentesque, finibus scelerisque dolor. Mauris sed laoreet dui. Integer maximus ullamcorper volutpat. Vestibulum feugiat libero eros, eget dapibus felis commodo vitae. Morbi ut metus lobortis, egestas erat sed, elementum leo. Aliquam molestie nunc a suscipit fermentum. Aliquam fermentum sed lectus ut fringilla. Integer eget ante convallis, efficitur ex at, malesuada nulla. Sed cursus, magna nec gravida rutrum, velit magna auctor orci, id sodales dui elit eu lectus. Vivamus consequat nibh sit amet ultricies volutpat. 
+      </p>
+      <p>
+      Nullam ultrices lobortis est eu placerat. Vestibulum consectetur semper libero in efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas mollis lorem quis nunc porttitor, at feugiat odio facilisis. In bibendum magna sed velit ullamcorper elementum. Phasellus sollicitudin ligula eu dapibus cursus. Integer feugiat neque a sapien dignissim rhoncus vel eget ex. Nulla auctor, sem eu dapibus lacinia, erat diam sodales justo, tristique laoreet enim massa sed lorem. Fusce dictum egestas pellentesque. Nam eget quam vulputate, malesuada leo semper, pharetra nibh. Aenean tincidunt tortor in vulputate eleifend. Mauris leo lorem, scelerisque at placerat non, accumsan eu mi. Suspendisse vulputate, risus in pharetra maximus, libero mi semper diam, eget tempor turpis ante ac nunc. Aliquam non tristique odio, in blandit ipsum. Aliquam id lectus malesuada, dictum nisl quis, condimentum mauris. 
+      </p>
+    </div>
+  )
+};
+
+export default userOutput;
