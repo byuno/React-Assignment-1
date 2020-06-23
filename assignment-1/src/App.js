b44c1a90de6bb25a736154ad09c0c34af654637e
Bobby Uno <robert.y.uno@gmail.com>
2020-06-23 15:46:00 -0700

    added username state, passed it to UserOutput component

--- assignment-1/src/App.js
+++ assignment-1/src/App.js
@@ -5,13 +5,21 @@ import UserOutput from './Components/UserOutput'
 
 class App extends Component {
 
+  state = {
+    username: [
+      { name: 'Bobby'},
+      { name: 'Robert'},
+      { name: 'Yutaka'}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Assignment 1 </h1>
-        <UserOutput name="John" />
-        <UserOutput name="Jane" />
-        <UserOutput name="Jerry" />
+        <UserOutput name={this.state.username[0].name}/>
+        <UserOutput name={this.state.username[1].name}/>
+        <UserOutput name={this.state.username[2].name}/>
 
       </div>
     );
